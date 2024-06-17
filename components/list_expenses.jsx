c836baefbe7512faaa76885fdd19c074b23dc423
ANDRIAMIORA Ainamalala Lucky <luckyainamalalalucky@gmail.com>
2024-06-17 05:24:50 +0300

    feature: delete expense button added

--- components/list_expenses.jsx
+++ components/list_expenses.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function List_expenses({ user_id, new_expense, expenses, set_expenses }) {
+
+    const [deleted_expense, set_deleted_expense] = useState({})
 
     const fetch_data = async () => {
         const res = await fetch('/api/users?id=' + user_id, {
@@ -19,9 +21,25 @@ export default function List_expenses({ user_id, new_expense, expenses, set_expe
 
     }
 
+    const delete_expense = async(expense)=>{
+        const expense_id = await expense.expense_id
+        const res = await fetch('api/expenses?expense_id='+expense_id,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json',
+            },
+        })
+
+        if (!res.ok){
+
+        }else{
+            set_deleted_expense(expense)
+        }
+    }
+
     useEffect(() => {
         fetch_data()
-    }, [new_expense]);
+    }, [new_expense, deleted_expense]);
 
     return (
         <>
@@ -32,20 +50,20 @@ export default function List_expenses({ user_id, new_expense, expenses, set_expe
                         {/* head */}
                         <thead>
                             <tr>
-                                <th></th>
                                 <th>expense</th>
                                 <th>value</th>
                                 <th>price</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             {
                                 expenses.map((expense, key) => (
                                     <tr key={key}>
-                                        <th>{expense.expense_id}</th>
                                         <td>{expense.expense_name}</td>
                                         <td>{expense.value}</td>
                                         <td>{expense.price}</td>
+                                        <td><button onClick={()=>delete_expense(expense)} className="btn btn-sm btn-warning">del</button></td>
                                     </tr>
                                 ))
                             }
